3b9202a419bd4e98b090c2b8a54d6c502f57b2ad
Mitsui <todays.mitsui@gmail.com>
2021-09-03 22:26:40 +0900

    feat: index

--- rocket-gcd/src/main.rs
+++ rocket-gcd/src/main.rs
@@ -1,8 +1,18 @@
 #[macro_use] extern crate rocket;
+use rocket::response::content;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello,world!"
+fn index() -> content::Html<&'static str> {
+    let body = r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="POST">
+            <input type="number" name="m">
+            <input type="number" name="n">
+            <button>Compute GCD</button>
+        </form>
+    "#;
+
+    content::Html(body)
 }
 
 #[launch]
