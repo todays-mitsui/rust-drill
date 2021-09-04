3862d7a832b4cb448385c5e42313be40a467fbe4
Mitsui <todays.mitsui@gmail.com>
2021-09-04 17:27:00 +0900

    feat: gcd()

--- rocket-gcd/src/main.rs
+++ rocket-gcd/src/main.rs
@@ -2,6 +2,11 @@
 use rocket::form;
 use rocket::response::content;
 
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, post_gcd])
+}
+
 #[get("/")]
 fn index() -> content::Html<&'static str> {
     let body = r#"
@@ -27,7 +32,27 @@ fn post_gcd(operands: form::Form<GcdOperands>) -> String {
     format!("m = {}, n = {}", operands.m, operands.n)
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, post_gcd])
+fn gcd(mut m: u64, mut n: u64) -> u64 {
+    assert!(m != 0 && n != 0);
+
+    while n != 0 {
+        if n < m {
+            // swap
+            let t = m;
+            m = n;
+            n = t;
+        }
+        n = n % m;
+    }
+    m
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(
+        gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19),
+        3 * 11
+    );
 }
