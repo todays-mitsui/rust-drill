2aff1cd7e21c7307133bc4760aa51b29dadc8dec
Mitsui <todays.mitsui@gmail.com>
2021-09-04 21:30:22 +0900

    feat: write_image

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,12 +1,11 @@
 extern crate num;
+extern crate image;
 
+use std::io::Write;
 use std::str::FromStr;
+use std::fs::File;
 use num::Complex;
-
-fn main() {
-    let c = Complex { re: 0.5, im: 0.5 };
-    println!("{:?}", complex_square_add_loop(c, 128));
-}
+use image::png::PNGEncoder;
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -85,21 +84,70 @@ fn test_pixel_to_point() {
 }
 
 fn render(
-    pixcels: &mut [u8],
+    pixels: &mut [u8],
     bounds: (usize, usize),
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
 ) {
-    assert!(pixcels.len() == bounds.0 * bounds.1);
+    assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for row in 0 .. bounds.0 {
-        for column in 0 .. bounds.1 {
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
 
-            pixcels[row * bounds.0 + column] = match escape_time(point, 255) {
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - (count as u8),
             };
         }
     }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        image::ColorType::Gray(8),
+    )?;
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(
+            std::io::stderr(),
+            "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT"
+        ).unwrap();
+
+        writeln!(
+            std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        ).unwrap();
+
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
