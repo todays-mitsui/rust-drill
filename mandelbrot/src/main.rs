5f7bdee17964991f82a8cfe2d309a89c2c088c6e
Mitsui <todays.mitsui@gmail.com>
2021-09-04 20:07:05 +0900

    feat: pixel_to_point

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -61,3 +61,25 @@ fn test_parse_complex() {
         None
     );
 }
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixcel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + (pixcel.0 as f64) * width / (bounds.0 as f64),
+        im: upper_left.im - (pixcel.1 as f64) * height / (bounds.1 as f64),
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point((100, 100), (25, 75), Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0}),
+        Complex { re: -0.5, im: -0.5 }
+    );
+}
