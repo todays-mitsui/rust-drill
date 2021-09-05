db4870aea8dce6ea01adec08b3e55543a9c6a295
Mitsui <todays.mitsui@gmail.com>
2021-09-05 13:50:06 +0900

    feat: (usize, usize) をやめて Bounds 型, Point 型を作った

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -61,41 +61,72 @@ fn test_parse_complex() {
     );
 }
 
+#[derive(Eq, PartialEq, Debug)]
+struct Bounds {
+    width: usize,
+    height: usize,
+}
+
+impl From<(u64, u64)> for Bounds {
+    fn from(pair: (u64, u64)) -> Bounds {
+        Bounds { width: pair.0 as usize, height: pair.1 as usize }
+    }
+}
+
+#[test]
+fn test_bounds_from_tuple() {
+    assert_eq!(
+        Bounds::from((42, 100)),
+        Bounds { width: 42, height: 100 }
+    );
+}
+
+#[derive(Eq, PartialEq, Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
 fn pixel_to_point(
-    bounds: (usize, usize),
-    pixcel: (usize, usize),
+    bounds: &Bounds,
+    pixcel: &Point,
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
 ) -> Complex<f64> {
     let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
 
     Complex {
-        re: upper_left.re + (pixcel.0 as f64) * width / (bounds.0 as f64),
-        im: upper_left.im - (pixcel.1 as f64) * height / (bounds.1 as f64),
+        re: upper_left.re + (pixcel.x as f64) * width / (bounds.width as f64),
+        im: upper_left.im - (pixcel.y as f64) * height / (bounds.height as f64),
     }
 }
 
 #[test]
 fn test_pixel_to_point() {
     assert_eq!(
-        pixel_to_point((100, 100), (25, 75), Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0}),
+        pixel_to_point(
+            &Bounds { width: 100, height: 100 },
+            &Point { x: 25, y: 75 },
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
         Complex { re: -0.5, im: -0.5 }
     );
 }
 
 fn render(
     pixels: &mut [u8],
-    bounds: (usize, usize),
+    bounds: &Bounds,
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
 ) {
-    assert!(pixels.len() == bounds.0 * bounds.1);
+    assert!(pixels.len() == bounds.width * bounds.height);
 
-    for row in 0 .. bounds.1 {
-        for column in 0 .. bounds.0 {
-            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+    for row in 0 .. bounds.height {
+        for column in 0 .. bounds.width {
+            let point = pixel_to_point(&bounds, &Point { x: column, y: row }, upper_left, lower_right);
 
-            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+            pixels[row * bounds.width + column] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - (count as u8),
             };
@@ -103,14 +134,14 @@ fn render(
     }
 }
 
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+fn write_image(filename: &str, pixels: &[u8], bounds: Bounds) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
 
     let encoder = PNGEncoder::new(output);
     encoder.encode(
         &pixels,
-        bounds.0 as u32,
-        bounds.1 as u32,
+        bounds.width as u32,
+        bounds.height as u32,
         image::ColorType::Gray(8),
     )?;
 
@@ -135,8 +166,9 @@ fn main() {
         std::process::exit(1);
     }
 
-    let bounds = parse_pair(&args[2], 'x')
+    let pair = parse_pair(&args[2], 'x')
         .expect("error parsing image dimensions");
+    let bounds = Bounds::from(pair);
 
     let upper_left = parse_complex(&args[3])
         .expect("error parsing upper left corner point");
@@ -144,26 +176,26 @@ fn main() {
     let lower_right = parse_complex(&args[4])
         .expect("error parsing lower right corner point");
 
-    let mut pixels = vec![0; bounds.0 * bounds.1];
+    let mut pixels = vec![0; bounds.width * bounds.height];
 
     // render(&mut pixels, bounds, upper_left, lower_right);
 
     let threads = 8;
-    let rows_per_band = bounds.1 / threads + 1;
+    let rows_per_band = bounds.height / threads + 1;
 
     {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.width).collect();
 
         crossbeam::scope(|spawner| {
             for (i, band) in bands.into_iter().enumerate() {
                 let top = rows_per_band * i;
-                let height = band.len() / bounds.0;
-                let band_bounds = (bounds.0, height);
-                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                let height = band.len() / bounds.width;
+                let band_bounds = Bounds { width: bounds.width, height };
+                let band_upper_left = pixel_to_point(&bounds, &Point { x: 0, y: top }, upper_left, lower_right);
+                let band_lower_right = pixel_to_point(&bounds, &Point { x: bounds.width, y: top + height }, upper_left, lower_right);
 
                 spawner.spawn(move || {
-                    render(band, band_bounds, band_upper_left, band_lower_right);
+                    render(band, &band_bounds, band_upper_left, band_lower_right);
                 });
             }
         });
