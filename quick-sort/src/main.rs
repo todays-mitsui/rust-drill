dfe13a041d84d4071621268258958a444b02074b
Mitsui <todays.mitsui@gmail.com>
2021-10-23 11:18:12 +0900

    feat: quick-sort v2

--- quick-sort/src/main.rs
+++ quick-sort/src/main.rs
@@ -1,19 +1,19 @@
 use std::cmp::Ordering;
 
-fn sort(xs: Vec<isize>) -> Vec<isize> {
+fn sort<T: Ord + Clone>(xs: Vec<T>) -> Vec<T> {
     if xs.len() == 0 { return Vec::new(); }
 
-    let pivot = xs[0];
+    let pivot = &xs[0];
 
     let mut less = Vec::new();
     let mut equal = Vec::new();
     let mut greater = Vec::new();
 
-    for x in xs {
+    for x in &xs {
         match x.cmp(&pivot) {
-            Ordering::Less => less.push(x),
-            Ordering::Equal => equal.push(x),
-            Ordering::Greater => greater.push(x),
+            Ordering::Less => less.push((*x).clone()),
+            Ordering::Equal => equal.push((*x).clone()),
+            Ordering::Greater => greater.push((*x).clone()),
         }
     }
 
